6365c4ac5d15954406808ddec7a33dfbb97d8bfd
Sourish Roy <sourish.roy1973@gmail.com>
2024-05-21 07:11:59 +0000

    setup react router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,67 @@
 import React from "react";
 
+import {
+  HomeLayout,
+  Landing,
+  About,
+  Cart,
+  Checkout,
+  Orders,
+  Products,
+  SingleProduct,
+  Error,
+  Login,
+  Register,
+} from "./pages";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "product/:id",
+        element: <SingleProduct />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router}>App</RouterProvider>;
 }
 
 export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function HomeLayout() {
+  return (
+    <div>
+      <Outlet />
+      HomeLayout
+    </div>
+  );
+}
+
+export default HomeLayout;
