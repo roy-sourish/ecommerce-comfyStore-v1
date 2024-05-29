0ebb85ac8ccde57055d16e5d08502a11371e347e
Sourish Roy <sourish.roy1973@gmail.com>
2024-05-29 08:26:01 +0000

    format price

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import { formatPrice } from "../utils/indexAxios";
 
 function ProductsGrid() {
   const { products } = useLoaderData();
@@ -8,6 +9,7 @@ function ProductsGrid() {
     <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
       {products.map((product) => {
         const { title, price, image } = product.attributes;
+        const dollarsAmount = formatPrice(price);
         return (
           <Link
             key={product.id}
@@ -23,7 +25,7 @@ function ProductsGrid() {
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title capitalize tracking-wider">{title}</h2>
-              <span className="text-secondary">{price}</span>
+              <span className="text-secondary">{dollarsAmount}</span>
             </div>
           </Link>
         );
